618167ebbfcc997ae08505d7b270839050846f84
itsjavedpatel <137042175+itsjavedpatel@users.noreply.github.com>
2025-02-16 03:01:44 +0530

    Created Models

---
+++ backend/models/Student.js
@@ -0,0 +1,59 @@
+const mongoose = require("mongoose");
+const University = require("./University");
+
+const studentSchema = new mongoose.Schema({
+  userName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+
+  password: {
+    type: String,
+  },
+
+  contact: {
+    type: String,
+  },
+  studentId: {
+    type: String,
+    required: true,
+  },
+  laundryId: {
+    type: String,
+    required: true,
+  },
+  hostel: {
+    type: String,
+  },
+  university: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "University",
+  },
+  roomNo: {
+    type: Number,
+  },
+  orders: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Order",
+    },
+  ],
+  role: {
+    type: String,
+    default: "student",
+    immutable: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+const Student = mongoose.model("Student", studentSchema);
+module.exports = User;
