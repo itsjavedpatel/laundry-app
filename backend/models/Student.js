6000b34ba5963bbb644aed1f85e76281383fd54e
itsjavedpatel <137042175+itsjavedpatel@users.noreply.github.com>
2025-02-16 19:22:14 +0530

    db-connected

--- backend/models/Student.js
+++ backend/models/Student.js
@@ -1,59 +1,58 @@
 const mongoose = require("mongoose");
 const University = require("./University");
 
-const studentSchema = new mongoose.Schema({
-  userName: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-  },
-
-  password: {
-    type: String,
-  },
-
-  contact: {
-    type: String,
-  },
-  studentId: {
-    type: String,
-    required: true,
-  },
-  laundryId: {
-    type: String,
-    required: true,
-  },
-  hostel: {
-    type: String,
-  },
-  university: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "University",
-  },
-  roomNo: {
-    type: Number,
-  },
-  orders: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Order",
+const studentSchema = new mongoose.Schema(
+  {
+    userName: {
+      type: String,
+      required: true,
+      trim: true,
     },
-  ],
-  role: {
-    type: String,
-    default: "student",
-    immutable: true,
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+
+    password: {
+      type: String,
+    },
+
+    contact: {
+      type: String,
+    },
+    studentId: {
+      type: String,
+      required: true,
+    },
+    laundryId: {
+      type: String,
+      required: true,
+    },
+    hostel: {
+      type: String,
+    },
+    university: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "University",
+    },
+    roomNo: {
+      type: Number,
+    },
+    orders: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Order",
+      },
+    ],
+    role: {
+      type: String,
+      default: "student",
+      immutable: true,
+    },
   },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
+  { timestamps: true }
+);
 const Student = mongoose.model("Student", studentSchema);
 module.exports = User;
--- backend/models/University.js
+++ backend/models/University.js
@@ -1,59 +1,58 @@
 const mongoose = require("mongoose");
-const universitySchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  address: {
-    type: String,
-  },
-  zipcode: {
-    type: Number,
-    required: true,
-    trim: true,
-  },
-  role: {
-    type: String,
-    default: "university",
-    immutable: true,
-  },
-  students: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Student",
+const universitySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
     },
-  ],
-  laundries: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Laundry",
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
     },
-  ],
-  delivery: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Delivery",
+    password: {
+      type: String,
+      required: true,
     },
-  ],
-  isSubscribed: {
-    type: Boolean,
-    default: false,
+    address: {
+      type: String,
+    },
+    zipcode: {
+      type: Number,
+      required: true,
+      trim: true,
+    },
+    role: {
+      type: String,
+      default: "university",
+      immutable: true,
+    },
+    students: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Student",
+      },
+    ],
+    laundries: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Laundry",
+      },
+    ],
+    delivery: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Delivery",
+      },
+    ],
+    isSubscribed: {
+      type: Boolean,
+      default: false,
+    },
   },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
+  { timestamps: true }
+);
 const University = mongoose.model("University", universitySchema);
 module.exports = University;
